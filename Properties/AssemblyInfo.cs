f5dc4f4a4ab0996067c25dbd0e57e13b78306293
SUP2Ak <cormier.wesley@gmail.com>
2023-12-27 06:31:37 +0100

    feat(Properties): add AssemblyInfo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csharp_script")]
+[assembly: AssemblyDescription('A simple c# boilerplate to create script on fivem')]
+[assembly: AssemblyConfiguration('')]
+[assembly: AssemblyCompany('SUP2Ak')]
+[assembly: AssemblyProduct('https://github.com/SUP2Ak/csharp-boilerplate-template')]
+[assembly: AssemblyCopyright('Copyright © 2023')]
+[assembly: AssemblyTrademark('')]
+[assembly: AssemblyCulture('')]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')]
+
+[assembly: AssemblyVersion('1.0.1')]
+[assembly: AssemblyFileVersion('1.0.1')]
